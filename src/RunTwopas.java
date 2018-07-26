c6d26f565f78f60b80e0a918756638f746dce4f8
Ana Moreno <atmoreno@users.noreply.github.com>
2018-07-26 14:33:42 +0200

    Added code to run twopas

--- src/RunTwopas.java
+++ src/RunTwopas.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class RunTwopas {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main60minutes(String[] args) throws IOException, InterruptedException {
 
         BufferedReader inputList = new BufferedReader(new FileReader(args[0]));
         String list = null;
@@ -23,7 +23,40 @@ public class RunTwopas {
             System.out.println();
 
             int p = 0;
-            while ((line = br.readLine()) != null & p < 22) {
+            while ((line = br.readLine()) != null & p < 46 ) {
+                System.out.println(line);
+                p++;
+            }
+            br.close();
+            process.destroy();
+
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main15minutes(String[] args) throws IOException, InterruptedException {
+
+        BufferedReader inputList = new BufferedReader(new FileReader(args[0]));
+        String list = null;
+        ArrayList<String> listFiles = new ArrayList<>();
+        while ((list = inputList.readLine())!= null) {
+            listFiles.add(list);
+        }
+        System.out.println("Number of scenarios to execute: " + listFiles.size());
+        for (String arg : listFiles){
+            Process process = new ProcessBuilder("TwoPas.exe ", arg).start();
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+
+            System.out.printf("Output of running %s is:", arg);
+            System.out.println();
+
+            int p = 0;
+            while ((line = br.readLine()) != null & p < 22 ) {
                 System.out.println(line);
                 p++;
             }
